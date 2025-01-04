03ee75278e541fdbbd7ecc02def96ec7b5c4e673
kshala <28948763+kshala@users.noreply.github.com>
2025-01-04 22:37:42 +0000

    Improving config flow for serial connection.

--- custom_components/sungrow/config_flow.py
+++ custom_components/sungrow/config_flow.py
@@ -6,6 +6,7 @@ from typing import Any
 import voluptuous as vol
 
 from homeassistant.config_entries import ConfigFlow, ConfigFlowResult
+from homeassistant.data_entry_flow import section
 from homeassistant.helpers.selector import (
     SelectSelector,
     SelectSelectorConfig,
@@ -15,27 +16,44 @@ from homeassistant.helpers.selector import (
 from .const import (
     CONF_CONNECTION_TYPE,
     CONF_CONNECTION_TYPE_SERIAL,
-    CONF_CONNECTION_TYPE_SERIAL_BAUDRATE,
-    CONF_CONNECTION_TYPE_SERIAL_BYTESIZE,
-    CONF_CONNECTION_TYPE_SERIAL_METHOD,
-    CONF_CONNECTION_TYPE_SERIAL_PARITY,
-    CONF_CONNECTION_TYPE_SERIAL_PORT,
-    CONF_CONNECTION_TYPE_SERIAL_STOPBITS,
     CONF_CONNECTION_TYPE_TCP,
-    CONF_CONNECTION_TYPE_TCP_HOST,
-    CONF_CONNECTION_TYPE_TCP_PORT,
     CONF_DEVICE_NAME,
     CONF_DEVICE_TYPE,
     CONF_MODBUS_ADDRESS,
+    CONF_SERIAL_BAUDRATE,
+    CONF_SERIAL_BYTESIZE,
+    CONF_SERIAL_EXTRAS,
+    CONF_SERIAL_METHOD,
+    CONF_SERIAL_PARITY,
+    CONF_SERIAL_PORT,
+    CONF_SERIAL_STOPBITS,
+    CONF_TCP_HOST,
+    CONF_TCP_PORT,
     DEFAULTS,
     DOMAIN,
     ENTITIES,
-    ENTITY_DEVICE_TYPE_CODE,
+    ENTITY_MODEL_NAME,
+    ENTITY_SERIAL_NUMBER,
     OPTIONS,
 )
-from .modbus_device import ModbusDevice, ModbusSerialDeviceConfig, ModbusTcpDeviceConfig
+from .modbus_device import (
+    DataType,
+    ModbusDevice,
+    ModbusSerialDeviceConfig,
+    ModbusTcpDeviceConfig,
+)
 
 _LOGGER = logging.getLogger(__name__)
+
+
+class VerifyDeviceConfigResult:
+    """Result of verifying the device configuration."""
+
+    def __init__(self, model_name: str, serial_number: str, error: str) -> None:
+        """Initialize the result."""
+        self.model_name = model_name
+        self.serial_number = serial_number
+        self.error = error
 
 
 class SungrowConfigFlow(ConfigFlow, domain=DOMAIN):
@@ -44,10 +62,11 @@ class SungrowConfigFlow(ConfigFlow, domain=DOMAIN):
     VERSION = 1
     MINOR_VERSION = 0
 
-    connection_type = None
+    def __init__(self) -> None:
+        """Initialize the config flow."""
 
-    tcp_config: ModbusTcpDeviceConfig = None
-    serial_config: ModbusSerialDeviceConfig = None
+        self.connection_type: str = None
+        self.config: ModbusTcpDeviceConfig | ModbusSerialDeviceConfig = None
 
     async def async_step_user(
         self, user_input: dict[str, Any] | None = None
@@ -100,49 +119,72 @@ class SungrowConfigFlow(ConfigFlow, domain=DOMAIN):
     ) -> ConfigFlowResult:
         """Handle connection type TCP."""
 
-        if self.tcp_config is None:
-            self.tcp_config = ModbusTcpDeviceConfig(
-                name=user_input.get(CONF_DEVICE_NAME),
-                device_type=user_input.get(CONF_DEVICE_TYPE),
-                host=user_input.get(CONF_CONNECTION_TYPE_TCP_HOST),
-                port=user_input.get(CONF_CONNECTION_TYPE_TCP_PORT),
-                modbus_address=user_input.get(CONF_MODBUS_ADDRESS),
+        device_name = user_input.get(CONF_DEVICE_NAME)
+        device_type = user_input.get(CONF_DEVICE_TYPE)
+        if self.config is None:
+            self.config = ModbusTcpDeviceConfig(
+                name=device_name,
+                device_type=device_type,
+                host=DEFAULTS[CONF_TCP_HOST],
+                port=DEFAULTS[CONF_TCP_PORT],
+                modbus_address=DEFAULTS[CONF_MODBUS_ADDRESS][device_type][
+                    CONF_CONNECTION_TYPE_TCP
+                ],
             )
 
-        if self.tcp_config.host is None:
-            self.tcp_config.host = user_input.get(CONF_CONNECTION_TYPE_TCP_HOST)
+        host = user_input.get(CONF_TCP_HOST)
+        port = user_input.get(CONF_TCP_PORT)
+        modbus_address = user_input.get(CONF_MODBUS_ADDRESS)
 
-        if self.tcp_config.port is None:
-            self.tcp_config.port = user_input.get(CONF_CONNECTION_TYPE_TCP_PORT)
+        if host is not None:
+            self.config.host = host
 
-        if self.tcp_config.modbus_address is None:
-            self.tcp_config.modbus_address = user_input.get(CONF_MODBUS_ADDRESS)
+        if port is not None:
+            self.config.port = port
+
+        if modbus_address is not None:
+            self.config.modbus_address = modbus_address
 
         errors: dict[str, str] = None
 
-        if self.tcp_config.iscomplete():
-            modbus_device: ModbusDevice = ModbusDevice(self.tcp_config)
-            errors = await self.async_test_device(modbus_device)
+        if self.config.iscomplete():
+            verify_result = await self.async_verify_device_config()
+            if verify_result.error is None:
+                return self.async_create_entry(
+                    title=self.config.name,
+                    data={
+                        CONF_DEVICE_NAME: self.config.name,
+                        CONF_DEVICE_TYPE: self.config.device_type,
+                        CONF_CONNECTION_TYPE: CONF_CONNECTION_TYPE_TCP,
+                        CONF_TCP_HOST: self.config.host,
+                        CONF_TCP_PORT: self.config.port,
+                        CONF_MODBUS_ADDRESS: self.config.modbus_address,
+                    },
+                )
 
-        if self.tcp_config.host is None or errors is not None:
+            if verify_result.error is not None:
+                errors = {"base": verify_result.error}
+
+        if not self.config.iscomplete() or errors is not None:
             return self.async_show_form(
                 data_schema=vol.Schema(
                     {
-                        vol.Required(CONF_CONNECTION_TYPE_TCP_HOST): str,
                         vol.Required(
-                            CONF_CONNECTION_TYPE_TCP_PORT,
-                            default=DEFAULTS[CONF_CONNECTION_TYPE_TCP_PORT],
+                            CONF_TCP_HOST,
+                            default=self.config.host,
+                        ): str,
+                        vol.Required(
+                            CONF_TCP_PORT,
+                            default=self.config.port,
                         ): vol.Coerce(int),
                         vol.Required(
                             CONF_MODBUS_ADDRESS,
-                            default=DEFAULTS[CONF_MODBUS_ADDRESS][
-                                self.tcp_config.device_type
-                            ][CONF_CONNECTION_TYPE_TCP],
+                            default=self.config.modbus_address,
                         ): vol.Coerce(int),
                     }
                 ),
                 errors=errors,
-                last_step=False,
+                last_step=True,
             )
 
         return None
@@ -152,107 +194,102 @@ class SungrowConfigFlow(ConfigFlow, domain=DOMAIN):
     ) -> ConfigFlowResult:
         """Handle connection type serial."""
 
-        if self.serial_config is None:
-            self.serial_config = ModbusSerialDeviceConfig(
+        if self.config is None:
+            self.config = ModbusSerialDeviceConfig(
                 name=user_input.get(CONF_DEVICE_NAME),
                 device_type=user_input.get(CONF_DEVICE_TYPE),
-                serial_port=user_input.get(CONF_CONNECTION_TYPE_SERIAL_PORT),
-                baudrate=user_input.get(CONF_CONNECTION_TYPE_SERIAL_BAUDRATE),
-                method=user_input.get(CONF_CONNECTION_TYPE_SERIAL_METHOD),
-                bytesize=user_input.get(CONF_CONNECTION_TYPE_SERIAL_BYTESIZE),
-                stopbits=user_input.get(CONF_CONNECTION_TYPE_SERIAL_STOPBITS),
-                parity=user_input.get(CONF_CONNECTION_TYPE_SERIAL_PARITY),
-                modbus_address=user_input.get(CONF_MODBUS_ADDRESS),
+                serial_port=None,
+                baudrate=None,
+                method=None,
+                bytesize=None,
+                stopbits=None,
+                parity=None,
+                modbus_address=None,
             )
 
-        if self.serial_config.serial_port is None:
-            self.serial_config.serial_port = user_input.get(
-                CONF_CONNECTION_TYPE_SERIAL_PORT
+        if self.config.serial_port is None:
+            self.config.serial_port = user_input.get(CONF_SERIAL_EXTRAS).get(
+                CONF_SERIAL_PORT
             )
 
-        if self.serial_config.baudrate is None:
-            self.serial_config.baudrate = user_input.get(
-                CONF_CONNECTION_TYPE_SERIAL_BAUDRATE
-            )
+        if self.config.baudrate is None:
+            self.config.baudrate = user_input.get(CONF_SERIAL_BAUDRATE)
 
-        if self.serial_config.method is None:
-            self.serial_config.method = user_input.get(
-                CONF_CONNECTION_TYPE_SERIAL_METHOD
-            )
+        if self.config.method is None:
+            self.config.method = user_input.get(CONF_SERIAL_METHOD)
 
-        if self.serial_config.bytesize is None:
-            self.serial_config.bytesize = user_input.get(
-                CONF_CONNECTION_TYPE_SERIAL_BYTESIZE
-            )
+        if self.config.bytesize is None:
+            self.config.bytesize = user_input.get(CONF_SERIAL_BYTESIZE)
 
-        if self.serial_config.stopbits is None:
-            self.serial_config.stopbits = user_input.get(
-                CONF_CONNECTION_TYPE_SERIAL_STOPBITS
-            )
+        if self.config.stopbits is None:
+            self.config.stopbits = user_input.get(CONF_SERIAL_STOPBITS)
 
-        if self.serial_config.parity is None:
-            self.serial_config.parity = user_input.get(
-                CONF_CONNECTION_TYPE_SERIAL_PARITY
-            )
+        if self.config.parity is None:
+            self.config.parity = user_input.get(CONF_SERIAL_PARITY)
 
-        if self.serial_config.modbus_address is None:
-            self.serial_config.modbus_address = user_input.get(CONF_MODBUS_ADDRESS)
+        if self.config.modbus_address is None:
+            self.config.modbus_address = user_input.get(CONF_MODBUS_ADDRESS)
 
-        if self.serial_config.serial_port is None:
+        if self.config.serial_port is None:
             return self.async_show_form(
                 data_schema=vol.Schema(
                     {
                         vol.Required(
-                            CONF_CONNECTION_TYPE_SERIAL_PORT,
-                            default=DEFAULTS[CONF_CONNECTION_TYPE_SERIAL_PORT],
+                            CONF_SERIAL_PORT,
+                            default=DEFAULTS[CONF_SERIAL_PORT],
                         ): vol.Coerce(int),
-                        vol.Required(
-                            CONF_CONNECTION_TYPE_SERIAL_BAUDRATE,
-                            default=DEFAULTS[CONF_CONNECTION_TYPE_SERIAL_BAUDRATE],
-                        ): vol.Coerce(int),
-                        vol.Required(
-                            CONF_CONNECTION_TYPE_SERIAL_METHOD,
-                            default=DEFAULTS[CONF_CONNECTION_TYPE_SERIAL_METHOD],
-                        ): SelectSelector(
-                            SelectSelectorConfig(
-                                options=OPTIONS[CONF_CONNECTION_TYPE_SERIAL_METHOD],
-                                mode=SelectSelectorMode.DROPDOWN,
-                                translation_key=CONF_CONNECTION_TYPE_SERIAL_METHOD,
-                                sort=False,
-                            )
-                        ),
-                        vol.Required(
-                            CONF_CONNECTION_TYPE_SERIAL_BYTESIZE,
-                            default=DEFAULTS[CONF_CONNECTION_TYPE_SERIAL_BYTESIZE],
-                        ): SelectSelector(
-                            SelectSelectorConfig(
-                                options=OPTIONS[CONF_CONNECTION_TYPE_SERIAL_BYTESIZE],
-                                mode=SelectSelectorMode.DROPDOWN,
-                                translation_key=CONF_CONNECTION_TYPE_SERIAL_BYTESIZE,
-                                sort=False,
-                            )
-                        ),
-                        vol.Required(
-                            CONF_CONNECTION_TYPE_SERIAL_STOPBITS,
-                            default=DEFAULTS[CONF_CONNECTION_TYPE_SERIAL_STOPBITS],
-                        ): SelectSelector(
-                            SelectSelectorConfig(
-                                options=OPTIONS[CONF_CONNECTION_TYPE_SERIAL_STOPBITS],
-                                mode=SelectSelectorMode.DROPDOWN,
-                                translation_key=CONF_CONNECTION_TYPE_SERIAL_STOPBITS,
-                                sort=False,
-                            )
-                        ),
-                        vol.Required(
-                            CONF_CONNECTION_TYPE_SERIAL_PARITY,
-                            default=DEFAULTS[CONF_CONNECTION_TYPE_SERIAL_PARITY],
-                        ): SelectSelector(
-                            SelectSelectorConfig(
-                                options=OPTIONS[CONF_CONNECTION_TYPE_SERIAL_PARITY],
-                                mode=SelectSelectorMode.DROPDOWN,
-                                translation_key=CONF_CONNECTION_TYPE_SERIAL_PARITY,
-                                sort=False,
-                            )
+                        CONF_SERIAL_EXTRAS: section(
+                            {"collapsed": True},
+                            {
+                                vol.Required(
+                                    CONF_SERIAL_BAUDRATE,
+                                    default=DEFAULTS[CONF_SERIAL_BAUDRATE],
+                                ): vol.Coerce(int),
+                                vol.Required(
+                                    CONF_SERIAL_METHOD,
+                                    default=DEFAULTS[CONF_SERIAL_METHOD],
+                                ): SelectSelector(
+                                    SelectSelectorConfig(
+                                        options=OPTIONS[CONF_SERIAL_METHOD],
+                                        mode=SelectSelectorMode.DROPDOWN,
+                                        translation_key=CONF_SERIAL_METHOD,
+                                        sort=False,
+                                    )
+                                ),
+                                vol.Required(
+                                    CONF_SERIAL_BYTESIZE,
+                                    default=DEFAULTS[CONF_SERIAL_BYTESIZE],
+                                ): SelectSelector(
+                                    SelectSelectorConfig(
+                                        options=OPTIONS[CONF_SERIAL_BYTESIZE],
+                                        mode=SelectSelectorMode.DROPDOWN,
+                                        translation_key=CONF_SERIAL_BYTESIZE,
+                                        sort=False,
+                                    )
+                                ),
+                                vol.Required(
+                                    CONF_SERIAL_STOPBITS,
+                                    default=DEFAULTS[CONF_SERIAL_STOPBITS],
+                                ): SelectSelector(
+                                    SelectSelectorConfig(
+                                        options=OPTIONS[CONF_SERIAL_STOPBITS],
+                                        mode=SelectSelectorMode.DROPDOWN,
+                                        translation_key=CONF_SERIAL_STOPBITS,
+                                        sort=False,
+                                    )
+                                ),
+                                vol.Required(
+                                    CONF_SERIAL_PARITY,
+                                    default=DEFAULTS[CONF_SERIAL_PARITY],
+                                ): SelectSelector(
+                                    SelectSelectorConfig(
+                                        options=OPTIONS[CONF_SERIAL_PARITY],
+                                        mode=SelectSelectorMode.DROPDOWN,
+                                        translation_key=CONF_SERIAL_PARITY,
+                                        sort=False,
+                                    )
+                                ),
+                            },
                         ),
                         vol.Required(
                             CONF_MODBUS_ADDRESS,
@@ -267,31 +304,36 @@ class SungrowConfigFlow(ConfigFlow, domain=DOMAIN):
 
         return None
 
-    async def async_test_device(
-        self, modbus_device: ModbusDevice
-    ) -> tuple[str, dict[str, str]]:
+    async def async_verify_device_config(self) -> VerifyDeviceConfigResult:
         """Test the connection to the Modbus device."""
 
-        device_type_code = ENTITIES[modbus_device.device_type][ENTITY_DEVICE_TYPE_CODE]
-        register = device_type_code["register"]
-        size = device_type_code["register_size"]
-
-        device_type = None
+        modbus_device = ModbusDevice(self.config)
+        device_type = self.config.device_type
+        model_name_value: str = None
+        serial_number_value: str = None
+        error_message: str = None
 
         try:
             await modbus_device.connect()
-            result = await modbus_device.read_input_register(register, count=size)
-            await modbus_device.close()
 
-            value = result[0]
+            entity_model_name = ENTITIES[device_type][ENTITY_MODEL_NAME]
+            register = entity_model_name["register"]
+            result = await modbus_device.read_int(register, data_type=DataType.UINT16)
+            model_name_value = entity_model_name["enum_values"][result]
 
-            device_type = device_type_code["data_values"][value]
+            entity_serial_number = ENTITIES[device_type][ENTITY_SERIAL_NUMBER]
+            register = entity_serial_number["register"]
+            register_size = entity_serial_number["register_size"]
+            result = await modbus_device.read_string(register, length=register_size)
+            serial_number_value = result
 
-            _LOGGER.debug("Device type code: %s", device_type)
-
+            _LOGGER.debug("Model name: %s", model_name_value)
         except Exception as exc:
-            message = f"Test Modbus device failed. {exc}"
-            _LOGGER.warning(message)
-            return [None, {"base": message}]
+            error_message = f"Test Modbus device failed. {exc}"
+            _LOGGER.warning(error_message)
 
-        return [device_type, None]
+        modbus_device.close()
+        modbus_device = None
+        return VerifyDeviceConfigResult(
+            model_name_value, serial_number_value, error_message
+        )
--- custom_components/sungrow/modbus_device.py
+++ custom_components/sungrow/modbus_device.py
@@ -1,41 +1,50 @@
 """ModbusDevice connects and communicates with a remote device either via TCP or Serial."""
 
+from enum import Enum
 import logging
+import struct
 
-from pymodbus.client import ModbusBaseClient
 from pymodbus.client.serial import AsyncModbusSerialClient
 from pymodbus.client.tcp import AsyncModbusTcpClient
+from pymodbus.constants import Endian
+from pymodbus.payload import BinaryPayloadDecoder
+from pymodbus.pdu import ModbusPDU
 
 _LOGGER = logging.getLogger(__name__)
+
+
+class RegisterType(Enum):
+    """Enumeration for Modbus register types."""
+
+    INPUT = 1
+    HOLDING = 2
+
+
+class DataType(str, Enum):
+    """Enumeration for Modbus data types."""
+
+    SINT16 = "sint16"
+    UINT16 = "uint16"
+    SINT32 = "sint32"
+    UINT32 = "uint32"
 
 
 class ModbusDeviceConfig:
     """Configuration for a Modbus device."""
 
-    name: str
-    device_type: str
-    modbus_address: int
-
     def __init__(self, *, name: str, device_type: str, modbus_address: int) -> None:
         """Initialize the Modbus device configuration."""
-        self.name = name
-        self.device_type = device_type
-        self.modbus_address = modbus_address
+        self.name: str = name
+        self.device_type: str = device_type
+        self.modbus_address: int = modbus_address
 
     def iscomplete(self) -> bool:
         """Check if the configuration is complete."""
-        return (
-            self.name is not None
-            and self.device_type is not None
-            and self.modbus_address is not None
-        )
+        return bool(self.name) and bool(self.device_type) and bool(self.modbus_address)
 
 
 class ModbusTcpDeviceConfig(ModbusDeviceConfig):
     """Configuration for a Modbus device connected via TCP."""
-
-    host: str
-    port: int
 
     def __init__(
         self, *, name: str, device_type: str, modbus_address: int, host: str, port: int
@@ -44,23 +53,16 @@ class ModbusTcpDeviceConfig(ModbusDeviceConfig):
         super().__init__(
             name=name, device_type=device_type, modbus_address=modbus_address
         )
-        self.host = host
-        self.port = port
+        self.host: str = host
+        self.port: int = port
 
     def iscomplete(self) -> bool:
         """Check if the configuration is complete."""
-        return super().iscomplete() and self.host is not None and self.port is not None
+        return super().iscomplete() and bool(self.host) and bool(self.port)
 
 
 class ModbusSerialDeviceConfig(ModbusDeviceConfig):
     """Configuration for a Modbus device connected via Serial."""
-
-    serial_port: str
-    baudrate: int
-    method: str
-    bytesize: int
-    stopbits: int
-    parity: str
 
     def __init__(
         self,
@@ -79,23 +81,23 @@ class ModbusSerialDeviceConfig(ModbusDeviceConfig):
         super().__init__(
             name=name, device_type=device_type, modbus_address=modbus_address
         )
-        self.serial_port = serial_port
-        self.baudrate = baudrate
-        self.method = method
-        self.bytesize = bytesize
-        self.stopbits = stopbits
-        self.parity = parity
+        self.serial_port: str = serial_port
+        self.baudrate: int = baudrate
+        self.method: str = method
+        self.bytesize: int = bytesize
+        self.stopbits: int = stopbits
+        self.parity: str = parity
 
     def iscomplete(self) -> bool:
         """Check if the configuration is complete."""
         return (
             super().iscomplete()
-            and self.serial_port is not None
-            and self.baudrate is not None
-            and self.method is not None
-            and self.bytesize is not None
-            and self.stopbits is not None
-            and self.parity is not None
+            and bool(self.serial_port)
+            and bool(self.baudrate)
+            and bool(self.method)
+            and bool(self.bytesize)
+            and bool(self.stopbits)
+            and bool(self.parity)
         )
 
 
@@ -105,9 +107,7 @@ class ModbusDevice:
     def __init__(self, config: ModbusDeviceConfig) -> None:
         """Initialize the ModbusDevice with the given configuration."""
 
-        # self._config: ModbusDeviceConfig = config
-        self._modbus_client: ModbusBaseClient = None
-
+        self._modbus_client: AsyncModbusSerialClient | AsyncModbusTcpClient = None
         self.device_address: int = config.modbus_address
         self.device_type: str = config.device_type
         self.device_name: str = config.name
@@ -134,12 +134,12 @@ class ModbusDevice:
 
         return await self._modbus_client.connect()
 
-    async def close(self) -> None:
+    def close(self) -> None:
         """Close the connection to the Modbus device."""
 
-        await self._modbus_client.close()
+        self._modbus_client.close()
 
-    async def write_holding_register(self, register: int, value: bytes | int) -> bool:
+    async def write_register(self, register: int, value: bytes | int) -> bool:
         """Write a value to a holding register."""
 
         result = await self._modbus_client.write_register(
@@ -150,14 +150,24 @@ class ModbusDevice:
             return False
         return not result.isError()
 
-    async def read_holding_register(
-        self, register: int, count: int = 1
+    async def read_register(
+        self,
+        register: int,
+        type=RegisterType.INPUT,
+        count=1,
     ) -> list[int] | None:
         """Read a holding register."""
 
-        result = await self._modbus_client.read_holding_registers(
-            address=register, count=count, slave=self.device_address
-        )
+        result: ModbusPDU = None
+        if type == RegisterType.INPUT:
+            result = await self._modbus_client.read_input_registers(
+                address=register, count=count, slave=self.device_address
+            )
+        else:
+            result = await self._modbus_client.read_holding_registers(
+                address=register, count=count, slave=self.device_address
+            )
+
         if result.isError():
             _LOGGER.warning(
                 "Failed to read Modbus register %s from %s",
@@ -167,19 +177,43 @@ class ModbusDevice:
             return None
         return result.registers
 
-    async def read_input_register(
-        self, register: int, count: int = 1
-    ) -> list[int] | None:
-        """Read an input register."""
+    async def read_string(
+        self,
+        register: int,
+        type=RegisterType.INPUT,
+        length=1,
+    ) -> str | None:
+        """Read a string from the Modbus device."""
 
-        result = await self._modbus_client.read_input_registers(
-            address=register, count=count, slave=self.device_address
-        )
-        if result.isError():
-            _LOGGER.warning(
-                "Failed to read Modbus register %s from %s",
-                register,
-                self.device_address,
-            )
+        # Read 16-bit words (2 bytes) since Modbus strings are typically stored as 16-bit characters
+        result = await self.read_register(register, type, length)
+
+        if result is not None:
+            decoder = BinaryPayloadDecoder.fromRegisters(result, byteorder=Endian.BIG)
+            decoded_string = decoder.decode_string(length * 2).decode("utf-8")
+            if "\x00" in decoded_string:
+                decoded_string = decoded_string[: decoded_string.index("\x00")]
+            return decoded_string
+        return None
+
+    async def read_int(
+        self,
+        register: int,
+        type=RegisterType.INPUT,
+        data_type=DataType.SINT16,
+    ) -> int | None:
+        """Read an integer from the Modbus device."""
+
+        count = 1 if data_type in [DataType.SINT16, DataType.UINT16] else 2
+        result = await self.read_register(register, type, count)
+
+        if result is None:
             return None
-        return result.registers
+
+        if count == 1:
+            fmt = ">h" if data_type == DataType.SINT16 else ">H"
+            return struct.unpack(fmt, struct.pack(">H", result[0]))[0]
+
+        value = (result[1] << 16) | result[0]
+        fmt = ">i" if data_type == DataType.SINT32 else ">I"
+        return struct.unpack(fmt, struct.pack(">I", value))[0]
